b3d63a47b1aae3c545c4f5a8e032599fc9e24098
orekhov-vitaly <orekhov.vitaly@gmail.com>
2025-11-10 17:30:10 +0100

    Fix after merged Homework07

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 // Импорты лекций
 // import Lesson06 from "lessons/Lesson06/Lesson06";
 // import Lesson07 from "lessons/Lesson07/Lesson07";
-import Lesson08 from "lessons/Lesson08/Lesson08";
+// import Lesson08 from "lessons/Lesson08/Lesson08";
 
 // Импорты домашних заданий
 // import Homework06 from "homeworks/Homework06/Homework06";
-import Homework07 from "homeworks/Homework07/Homework07";
+// import Homework07 from "homeworks/Homework07/Homework07";
 
 function App() {
     return (
@@ -14,12 +14,12 @@ function App() {
             { /* Лекция 6 - TypeScript */ }
             {/* <Lesson06/> */}
             {/* <Lesson07 /> */}
-            <Lesson08 />
+            {/* <Lesson08 /> */}
 
 
             {/* Homeworks */}
             {/* <Homework06 /> */}
-            <Homework07 />
+            {/* <Homework07 /> */}
         </>
     );
 }
